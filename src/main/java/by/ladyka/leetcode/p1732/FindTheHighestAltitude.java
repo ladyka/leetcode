86c7608921108a993b04fadc83b5aec41598b730
Andrei Ladyka <andrei@ladyka.by>
2023-06-19 19:31:41 +0300

    1732. Find the Highest Altitude

---
+++ src/main/java/by/ladyka/leetcode/p1732/FindTheHighestAltitude.java
@@ -0,0 +1,21 @@
+package by.ladyka.leetcode.p1732;
+
+/**
+ * 1732. Find the Highest Altitude
+ * <p>
+ * There is a biker going on a road trip. The road trip consists of n + 1 points at different altitudes. The biker starts his trip on point 0 with altitude equal 0.
+ * You are given an integer array gain of length n where gain[i] is the net gain in altitude between points i and i + 1 for all (0 <= i < n). Return the highest altitude of a point.
+ */
+public class FindTheHighestAltitude {
+    public int largestAltitude(int[] gain) {
+        int max = 0;
+        int current = 0;
+        for (int g : gain) {
+            current += g;
+            if (max < current) {
+                max = current;
+            }
+        }
+        return max;
+    }
+}
---
+++ src/test/java/by/ladyka/leetcode/p1732/FindTheHighestAltitudeTest.java
@@ -0,0 +1,14 @@
+package by.ladyka.leetcode.p1732;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class FindTheHighestAltitudeTest {
+    FindTheHighestAltitude findTheHighestAltitude = new FindTheHighestAltitude();
+
+    @Test
+    void largestAltitude() {
+        Assertions.assertEquals(1, findTheHighestAltitude.largestAltitude(new int[]{-5, 1, 5, 0, -7}));
+        Assertions.assertEquals(0, findTheHighestAltitude.largestAltitude(new int[]{-4, -3, -2, -1, 4, 3, 2}));
+    }
+}
