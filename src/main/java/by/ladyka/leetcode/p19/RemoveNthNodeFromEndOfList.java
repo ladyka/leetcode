adf3c562304967b12c9db893ea9a352ea2df9f3c
Andrei Ladyka <andrei@ladyka.by>
2024-03-03 19:08:56 +0300

    19. Remove Nth Node From End of List
    https://leetcode.com/problems/remove-nth-node-from-end-of-list/description/

---
+++ src/main/java/by/ladyka/leetcode/p19/RemoveNthNodeFromEndOfList.java
@@ -0,0 +1,43 @@
+package by.ladyka.leetcode.p19;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+public class RemoveNthNodeFromEndOfList {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode headCalculateLenght = head;
+        List<ListNode> nodes = new ArrayList<>(30);
+        do {
+            nodes.add(headCalculateLenght);
+            headCalculateLenght = headCalculateLenght.next;
+        } while (headCalculateLenght != null);
+
+
+        int positionToRemove = nodes.size() - n;
+        if (positionToRemove == 0) {
+            head = head.next;
+            return head;
+        }
+        ListNode previous = nodes.get(positionToRemove - 1);
+
+        int nextPosition = positionToRemove + 1;
+        if (nextPosition < nodes.size()) {
+            previous.next = nodes.get(nextPosition);
+            System.out.println(head);
+        } else {
+            previous.next = null;
+        }
+
+        return head;
+    }
+}
---
+++ src/test/java/by/ladyka/leetcode/p19/RemoveNthNodeFromEndOfListTest.java
@@ -0,0 +1,34 @@
+package by.ladyka.leetcode.p19;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class RemoveNthNodeFromEndOfListTest {
+    RemoveNthNodeFromEndOfList solution = new RemoveNthNodeFromEndOfList();
+
+    @Test
+    void removeNthFromEnd1() {
+        ListNode result = solution.removeNthFromEnd(ListNode.of(new int[]{1, 2, 3, 4, 5}), 2);
+        ListNode expected = ListNode.of(new int[]{1, 2, 3, 5});
+        assertListNodes(expected, result);
+    }
+    @Test
+    void removeNthFromEnd2() {
+        ListNode result = solution.removeNthFromEnd(ListNode.of(new int[]{1}), 1);
+        assert result == null;
+    }
+    @Test
+    void removeNthFromEnd3() {
+        ListNode result = solution.removeNthFromEnd(ListNode.of(new int[]{1, 2}), 1);
+        ListNode expected = ListNode.of(new int[]{1});
+        assertListNodes(expected, result);
+    }
+
+    private static void assertListNodes(ListNode expected, ListNode result) {
+        do {
+            Assertions.assertEquals(expected.val, result.val);
+            result = result.next;
+            expected = expected.next;
+        } while (result != null);
+    }
+}
