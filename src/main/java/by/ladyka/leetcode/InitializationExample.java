748e91606710d5ee9bbf9d15b1883bd2d26fb2ce
Andrei Ladyka <andrei@ladyka.by>
2024-03-08 11:50:42 +0300

    SETUP nexus URL

--- src/main/java/by/ladyka/leetcode/InitializationExample.java
+++ src/main/java/by/ladyka/leetcode/InitializationExample.java
@@ -5,21 +5,29 @@ public class InitializationExample {
         System.out.println("{2}");
     }
 
+    private String indicator = "Original {7}";
+    private static final InitializationExample initializationExample = new InitializationExample(" field {5}");
+
+    {
+        System.out.println("{1} " + indicator);
+    }
+
     static {
-        System.out.println("{4}");
+        System.out.println("{4}" + initializationExample.indicator);
     }
 
-    {
-        System.out.println("{1}");
-    }
 
     {
-        System.out.println("{3}");
+        System.out.println("{3} " + indicator);
     }
-    // NO !!! private static final InitializationExample = new InitializationExample();
+
+    public InitializationExample(String indicator) {
+        System.out.println("InitializationExample.constructor " + indicator);
+        this.indicator = indicator;
+    }
 
     public static void main(String[] args) {
         System.out.println("main()");
-        new InitializationExample();
+        new InitializationExample(" psvm {6}");
     }
 }
